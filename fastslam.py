468b28cde81ffff8b2afa062819ca7f2c29c10a7
Kwokfung Tang <tang.409@gmail.com>
2017-05-07 00:29:58 -0700

    argh

--- fastslam.py
+++ fastslam.py
@@ -1,26 +1,69 @@
 import numpy as np
 import random
+from particle import *
+from copy import deepcopy
 
 class FastSLAM:
     def __init__(self, init_pos, num_particles, landmarks):
         self.particles = []
-        self.robot = Robot(init_pos)
+        self.robot = Robot(init_pos, landmarks)
         self.landmarks = landmarks
         self.num_particles = num_particles
         self.create_particles()
 
     def create_particles(self):
-        pass
+        for i in range(self.num_particles):
+            p = Particle(self.robot.pose)
+            self.particles.append(p)
 
-    def run(self):
-        while True:
+    def run(self, controls):
+        for ctrl in controls:
+            self.robot.move(ctrl)
+            print self.robot.pose
             # receive robot controls
-            objs = self.robot.sense()
-            for p in self.particles:
-                p.update(objs)
-            self.particles = self.resample_particles()
+            obss = self.robot.sense()
+            print obss
+            for obs in obss:
+                for p in self.particles:
+                    p.update(obs)
+                print [p.weight for p in self.particles]
+                self.particles = self.resample_particles()
+            #print self.mean_pos()
 
     def resample_particles(self):
-        return self.particles
+        new_particles = []
+        weight = [p.weight for p in self.particles]
+        index = int(random.random() * self.num_particles)
+        beta = 0.0
+        mw = max(weight)
+        for i in range(self.num_particles):
+            beta += random.random() * 2.0 * mw
+            while beta > weight[index]:
+                beta -= weight[index]
+                index = (index + 1) % self.num_particles
+            new_particle = deepcopy(self.particles[index])
+            new_particle.weight = 1.0
+            new_particles.append(new_particle)
+        return new_particles
 
+    def mean_pos(self):
+        mean_x = 0.0
+        mean_y = 0.0
+        for p in self.particles:
+            mean_x += p.pose.x
+            mean_y += p.pose.y
+        return mean_x/self.num_particles, mean_y/self.num_particles
+
+if __name__=="__main__":
+    lms = []
+    for i in range(10):
+        lm = Landmark(mu=np.array([[i],[i]]), sigma=np.eye(2))
+        lms.append(lm)
+        lm = Landmark(mu=np.array([[i*math.cos(math.pi/6)],[i*math.sin(math.pi/6)]]), sigma=np.eye(2))
+        lms.append(lm)
+
+    p0 = Pose(0, 0, math.pi/4)
+    fs = FastSLAM(p0, 10, lms)
+    controls = [Pose(1,1,0),]*5
+    fs.run(controls)
 
--- particle.py
+++ particle.py
@@ -37,21 +37,21 @@ class Landmark:
     def __str__(self):
         return "Landmark mu: {}, sigma: {}".format(np.array_str(self.mu),  np.array_str(self.sigma))
 
-class Particle:
+class Particle(object):
     tol = 1e-4
 
     def __init__(self, pose):
         self.pose = pose
         self.landmarks = []
-        self.motion_noise = 0.2
-        self.theta_noise = 0.1
+        self.motion_noise = 0.1
+        self.theta_noise = 0.3
         self.obs_noise_cov = np.array([[0.1, 0], [0, (3.0*math.pi/180)**2]])
         self.weight = 1.0 
 
     def move_noise(self):
         x_noise = random.gauss(0, self.motion_noise)
         y_noise = random.gauss(0, self.motion_noise)
-        t_noise = random.gauss(0, self.theta_noise)
+        t_noise = random.gauss(0, self.theta_noise*math.pi/180)
         return Pose(x_noise, y_noise, t_noise)
 
     def move(self, mv):
@@ -79,9 +79,12 @@ class Particle:
     def jacobian(self, mu):
         dx = mu[0][0] - self.pose.x
         dy = mu[1][0] - self.pose.y
-        d2 = dx**2+dy**2
+        d2 = dx**2+dy**2+1e-60
         d = math.sqrt(d2)
-        pred_obs = np.array([[d], [math.atan2(dy, dx)]])
+        if abs(dx)<1e-60 and abs(dy)<1e-60:
+            pred_obs = np.array([[d], [self.pose.theta]])
+        else:
+            pred_obs = np.array([[d], [math.atan2(dy, dx)]])
         G = np.array([[dx/d, dy/d],[-dy/d2, dx/d2]])
         return pred_obs, G
 
@@ -112,7 +115,6 @@ class Particle:
         for i, landmark in enumerate(self.landmarks):
             p_obs, _G = self.jacobian(landmark.mu)
             _Q = _G.T.dot(landmark.sigma).dot(_G)+self.obs_noise_cov
-            print((obs-p_obs).T)
             w = mahalanobis(obs, p_obs, _Q)
             if w>prob:
                 prob = w
@@ -122,17 +124,69 @@ class Particle:
                 ix = i
         return prob, ix, pred_obs, G, Q
 
+class Robot(Particle):
+    def __init__(self, pose, map_landmarks, fov=math.pi/3, range=2):
+        super(Robot, self).__init__(pose)
+        self.map_landmarks = map_landmarks
+        self.fov = fov
+        self.range = range
+        self.distance_noise = 0.1
+        self.angle_noise = 0.5
+
+    def zero_noise(self):
+        self.distance_noise = 0.0
+        self.angle_noise = 0.0
+        self.motion_noise = 0.0
+        self.theta_noise = 0.0
+        self.obs_noise_cov = np.zeros((2,2))
+
+    def sense(self):
+        obs_list = []
+        for lm in self.map_landmarks:
+            flag, dist, angle = self.in_range(lm.mu)
+            if flag:
+                obs_list.append((dist, angle))
+        return obs_list
+
+    def in_range(self, mu):
+        pred_obs, _ = self.jacobian(mu)
+        dist = pred_obs[0][0] + random.gauss(0, self.distance_noise)
+        dist = max(0, dist)
+        flag = True 
+        if dist>self.range:
+            flag = False
+        angle = (pred_obs[1][0]+2*math.pi+ random.gauss(0, self.angle_noise*math.pi/180))%(2*math.pi)
+        diff = abs(angle-self.pose.theta)
+        if diff>math.pi:
+            diff = 2*math.pi-diff
+        if flag and diff>self.fov/2:
+            flag = False
+        #print flag, mu.ravel(), dist, pred_obs[1][0]*180/math.pi, angle*180/math.pi, diff*180/math.pi
+        return flag, dist, angle
+
+
 if __name__=="__main__":
     pose = Pose(0, 0, math.pi/4)
-    p = Particle(pose)
-    movement = Pose(1, 1, math.pi/20)
-    p.move(movement)
-    print(p.pose)
-    obs = (5, math.pi/9)
-    p.update(obs)
-    for lm in p.landmarks:
-        print(lm)
-    print(p.check_association(obs))
-    # cov = np.array([[ 0.18409051, 0.0315773 ],[ 0.0315773,0.01785957]])
-    # print("det:", np.linalg.det(cov))
-    # print("man:", mahalanobis(obs, obs, cov))
+    lms = []
+    for i in range(10):
+        lm = Landmark(mu=np.array([[i],[i]]), sigma=np.eye(2))
+        lms.append(lm)
+        lm = Landmark(mu=np.array([[i*math.cos(math.pi/6)],[i*math.sin(math.pi/6)]]), sigma=np.eye(2))
+        lms.append(lm)
+    robot = Robot(pose, lms, fov=2*math.pi/3)
+    #robot.zero_noise()
+    for i in range(5):
+        robot.move(Pose(1,1,0))
+        #print "Step {}: , pose: {}".format(str(i), robot.pose)
+        obss = robot.sense()
+        for obs in obss:
+            print obs, robot.g_inv(obs).ravel()
+    # p = Particle(pose)
+    # movement = Pose(1, 1, math.pi/20)
+    # p.move(movement)
+    # print(p.pose)
+    # obs = (5, math.pi/9)
+    # p.update(obs)
+    # for lm in p.landmarks:
+    #     print(lm)
+    # print(p.check_association(obs))
